ea1fdd8748d562f89a4df8b93f58af698ff09e29
eden-fenster <eden.fenster@gmail.com>
2024-09-02 09:09:56 +0300

    first commit

---
+++ bingo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import logging
+import sys
+from typing import List
+
+
+
+def main():
+    NUMBERS: int = 100
+    ROW: int = 5
+    COL: int = 5
+
+    bingo_board: List[List[int]] = [[1, 2, 3 ,4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]
+    for i in range(0, ROW):
+        for j in range(0, COL):
+            print (f'{bingo_board[i][j]}' + '\n')
+
+if __name__ == '__main__':
+    main()
+
+# def fill_list(number_list: List[int]):
+#     for i in range (0, 100):
+#         number_list[i] = i + 1
+#         is_marked[i] = False
+
+# def random_number() -> int:
+#     # return a random number between 1 - 100
+#     return 1
+
+# def mark_number():
+#     #mark number
+#     return 1
+
