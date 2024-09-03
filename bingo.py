f1c77b239f22104da820ec1349a775939eabbfdb
eden-fenster <eden.fenster@gmail.com>
2024-09-03 12:37:52 +0300

    test

--- bingo.py
+++ bingo.py
@@ -13,7 +13,8 @@ def main():
     bingo_board: List[List[int]] = [[1, 2, 3 ,4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]
     for i in range(0, ROW):
         for j in range(0, COL):
-            print (f'{bingo_board[i][j]}' + '\n')
+            print(f'{bingo_board[i][j]}' + '\n')
+            print('we now have a bingo board !\n')
 
 if __name__ == '__main__':
     main()
